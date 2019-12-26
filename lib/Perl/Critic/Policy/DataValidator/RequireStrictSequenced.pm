bab83aac939e6f6845bcf8e82fd76041508a0716
mp0liiu <raian@reeshome.org>
2019-12-27 04:47:43 +0900

    with の引数もparse_arg_listで解析

--- lib/Perl/Critic/Policy/DataValidator/RequireStrictSequenced.pm
+++ lib/Perl/Critic/Policy/DataValidator/RequireStrictSequenced.pm
@@ -24,53 +24,52 @@ my $EXPLAIN     = q{Passing named arguments when a subroutine has only one argum
 use DDP +{ deparse => 1, use_prototypes => 0 };
  
 sub violates {
-    my ($self, $stmt, $doc) = @_;
+  my ( $self, $stmt, $doc ) = @_;
 
-    return unless $stmt->symbols != 1;
+  return unless $stmt->symbols != 1;
 
-    my @tokens = $stmt->schildren;
+  my @tokens = $stmt->schildren;
 
-    # match 'state $v = Data::Validator->new()'
-    # XXX: このようなチェック、もっといい方法で行えないのか
-    if ( $tokens[0]->isa('PPI::Token::Word')
-        && $tokens[1]->isa('PPI::Token::Symbol')
-        && $tokens[2]->isa('PPI::Token::Operator')
-        && $tokens[2]->content eq '='
-        && $tokens[3]->isa('PPI::Token::Word')
-        && $tokens[3]->content eq 'Data::Validator'
-        && $tokens[4]->isa('PPI::Token::Operator')
-        && $tokens[4]->content eq '->'
-        && $tokens[5]->isa('PPI::Token::Word')
-        && $tokens[5]->content eq 'new'
-    ) {
-        my @args_passed_to_new = parse_arg_list($tokens[5]);
+  # match 'state $v = Data::Validator->new'
+  # XXX: このようなチェック、もっといい方法で行えないのか
+  if ( $tokens[0]->isa('PPI::Token::Word')
+    && $tokens[1]->isa('PPI::Token::Symbol')
+    && $tokens[2]->isa('PPI::Token::Operator')
+    && $tokens[2]->content eq '='
+    && $tokens[3]->isa('PPI::Token::Word')
+    && $tokens[3]->content eq 'Data::Validator'
+    && $tokens[4]->isa('PPI::Token::Operator')
+    && $tokens[4]->content eq '->'
+    && $tokens[5]->isa('PPI::Token::Word')
+    && $tokens[5]->content eq 'new' )
+  {
+    my @args_passed_to_new = parse_arg_list($tokens[5]);
 
-        # Args passed to new as named arguments
-        if ( @args_passed_to_new % 2 == 0 ) {
-            my $args_num = int @args_passed_to_new / 2;
+    # Args passed to new as named arguments
+    if ( @args_passed_to_new % 2 == 0 ) {
+      my $params_num = int @args_passed_to_new / 2;
 
-            # match '->with(qw/ /)'
-            # TODO: with を別に呼び出している場合
-            if ( $tokens[7]->isa('PPI::Token::Operator')
-                && $tokens[7]->content eq '->'
-                && $tokens[8]->isa('PPI::Token::Word')
-                && $tokens[8]->content eq 'with'
-                && $tokens[9]->isa('PPI::Structure::List')
-            ) {
-                my ($expression) = $tokens[9]->schildren;
-                my ($words)      = $expression->schildren;
-                my @role_names   = $words->literal;
-                my $has_strict_sequenced = any { $_ eq 'StrictSequenced' } @role_names;
-                if ($args_num == 1 && !$has_strict_sequenced) {
-                    return $self->violation($DESCRIPTION, $EXPLAIN, $stmt);
-                }
-                elsif ($args_num > 1 && $has_strict_sequenced) {
-                    return $self->violation($DESCRIPTION, $EXPLAIN, $stmt);
-                }
-            }
+      # match '->with(qw/ /)'
+      # TODO: with を別に呼び出している場合
+      if ( $tokens[7]->isa('PPI::Token::Operator')
+        && $tokens[7]->content eq '->'
+        && $tokens[8]->isa('PPI::Token::Word')
+        && $tokens[8]->content eq 'with'
+        && $tokens[9]->isa('PPI::Structure::List') )
+      {
+        my @args_passed_to_with  = parse_arg_list($tokens[8]);
+        my @extensions_name      = map { $_->literal } map { @$_ } @args_passed_to_with;
+        my $has_strict_sequenced = any { $_ eq 'StrictSequenced' } @extensions_name;
+        if ( $params_num == 1 && !$has_strict_sequenced ) {
+          return $self->violation($DESCRIPTION, $EXPLAIN, $stmt);
         }
+        elsif ( $params_num > 1 && $has_strict_sequenced ) {
+          return $self->violation($DESCRIPTION, $EXPLAIN, $stmt);
+        }
+      }
     }
-    return;
+  }
+  return;
 }
  
 1;
